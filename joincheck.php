79ed8116f78ecc67e539799753b67ec1d72ceb1e
SSUHan <joyangel93@naver.com>
2015-07-25 22:08:22 +0900

    joincheck.php 테이블 명령어 수정 - 서버 mysql 연결 완료

--- joincheck.php
+++ joincheck.php
@@ -11,7 +11,7 @@ if(!$conn = connect_mysqli("localhost","ljs93kr","wnddnjsQkd","ljs93kr")){
 	echo "connnection error!<br>";
 }
 else{
-echo "connect success!<br>";
+echo "connect success!1<br>";
 }
 $sql = "SELECT * FROM jp_user_info";
 
@@ -22,10 +22,13 @@ echo "query fail...<br>";
 
 
 if(joinOK($user_id,$result)){
-	$sql = "INSERT INTO usertable (pid, username, userpw, created) VALUES ('".$user_id."','".$user_name."','".$user_pw."',now())";
+	$sql = "INSERT INTO jp_user_info (userid, username, userpw, jointime) VALUES ('".$user_id."','".$user_name."','".$user_pw."',now())";
 	if(mysqli_query($conn,$sql)){
 		echo("<script>document.location.href='http://ljs93kr.cafe24.com/JPPage/join.html?mode=ok';</script>");
 
+	}
+	else{
+		echo "insert fail...<br>";
 	}
 	
 
